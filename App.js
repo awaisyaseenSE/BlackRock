0aa9a34b18d6abfd4b7857c6bd7417f215db73f5
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-01-30 14:28:04 +0500

    just create a basic project

---
+++ App.js
@@ -0,0 +1,14 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>App</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
