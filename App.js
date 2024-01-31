19a0b48c7a1330f15703096e16be5090900618ea
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-01-31 09:56:09 +0500

    setup firebase successfully in app

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
 import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
+import auth from '@react-native-firebase/auth';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView>
-        <View>
-          <Text>App is here</Text>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'beige'}}>
+          <Text>App is here ...</Text>
         </View>
       </SafeAreaView>
     </>
