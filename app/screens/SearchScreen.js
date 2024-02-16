384972f4227b23f61141ddbd234eb221f615ff2c
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-02-16 17:21:30 +0500

    make search for both movie and tv

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -14,6 +14,7 @@ import {
   ActivityIndicator,
   TouchableWithoutFeedback,
   Keyboard,
+  Modal,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import LinearGradient from 'react-native-linear-gradient';
@@ -31,6 +32,7 @@ import TextInputCompo from '../components/TextInputCompo';
 import ScreenComponent from '../components/ScreenComponent';
 import TextInputWithLeftIconCompo from '../components/TextInputWithLeftIconCompo';
 import LottieView from 'lottie-react-native';
+import {getResponsiveHeight} from '../utils/getResponsiveMarginPadding';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -41,10 +43,15 @@ export default function SearchScreen() {
   const navigation = useNavigation();
   const [searchText, setSearchText] = useState('');
   const [searchMovieData, setSearchMovieData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [isMovieSelected, setIsMovieSelected] = useState(true);
 
   const handleSearchMovies = async () => {
     let url = `/search/movie?query=${encodeURIComponent(searchText)}`;
-    let API_URL = `${constants.theMovieDb_BASE_URL}${url}&api_key=${constants.theMovieDb_API_KEY}`;
+    let tv_url = `/search/tv?query=${encodeURIComponent(searchText)}`;
+    let API_URL = `${constants.theMovieDb_BASE_URL}${
+      isMovieSelected ? url : tv_url
+    }&api_key=${constants.theMovieDb_API_KEY}`;
     try {
       setLoading(true);
       let response = await fetch(API_URL);
@@ -59,6 +66,7 @@ export default function SearchScreen() {
       // console.log('Response data:', allMoviesData?.length);
       if (responseData?.results?.length > 0) {
         setSearchMovieData(allMoviesData);
+        console.log(responseData);
       }
       setLoading(false);
     } catch (error) {
@@ -124,22 +132,43 @@ export default function SearchScreen() {
                   paddingHorizontal: 20,
                   paddingTop: Platform.OS === 'android' ? 10 : 0,
                 }}>
-                <TextInputWithLeftIconCompo
-                  value={searchText}
-                  onChangeText={text => {
-                    if (text.trim().length) {
-                      setSearchText(text);
-                    } else {
-                      setSearchText('');
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <TextInputWithLeftIconCompo
+                    value={searchText}
+                    onChangeText={text => {
+                      if (text.trim().length) {
+                        setSearchText(text);
+                      } else {
+                        setSearchText('');
+                      }
+                    }}
+                    maxLength={40}
+                    inputStyle={styles.inputStyle}
+                    clearIcon={searchText.length > 0 ? 'Clear' : ''}
+                    onPressClear={() => setSearchText('')}
+                    placeholder={
+                      isMovieSelected ? 'Search movies' : 'Search tv series'
                     }
-                  }}
-                  maxLength={40}
-                  inputStyle={styles.inputStyle}
-                  clearIcon={searchText.length > 0 ? 'Clear' : ''}
-                  onPressClear={() => setSearchText('')}
-                  placeholder="Search movies"
-                  placeholderTextColor="gray"
-                />
+                    placeholderTextColor="gray"
+                  />
+                  <TouchableOpacity
+                    style={{
+                      // height: getResponsiveHeight(6),
+                      alignItems: 'center',
+                      marginLeft: 20,
+                      justifyContent: 'center',
+                    }}
+                    onPress={() => setShowModal(true)}>
+                    <Image
+                      source={require('../assets/menu-burger.png')}
+                      style={styles.menuIcon}
+                    />
+                  </TouchableOpacity>
+                </View>
               </View>
               {loading && (
                 <View style={{marginBottom: 8, alignItems: 'center'}}>
@@ -173,6 +202,57 @@ export default function SearchScreen() {
               )} */}
             </View>
           </TouchableWithoutFeedback>
+          {showModal && (
+            <Modal visible={showModal} transparent animationType="slide">
+              <ScreenComponent style={{backgroundColor: 'rgba(0, 0, 0, 0.6)'}}>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  onPress={() => setShowModal(false)}
+                  style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <View style={styles.modalView}>
+                    <TouchableOpacity
+                      style={styles.modalCloseIconContainer}
+                      onPress={() => setShowModal(false)}>
+                      <Image
+                        source={require('../assets/close.png')}
+                        style={styles.modalCloseIcon}
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.modalViewContainer}
+                      onPress={() => setIsMovieSelected(true)}>
+                      <View
+                        style={
+                          isMovieSelected === true
+                            ? styles.fillRadio
+                            : styles.radio
+                        }
+                      />
+                      <Text style={styles.modalText}>Search Movie</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.modalViewContainer}
+                      onPress={() => setIsMovieSelected(false)}>
+                      <View
+                        style={
+                          isMovieSelected === false
+                            ? styles.fillRadio
+                            : styles.radio
+                        }
+                      />
+                      <Text style={styles.modalText}>
+                        Search TV Serial Movie
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </TouchableOpacity>
+              </ScreenComponent>
+            </Modal>
+          )}
         </ScreenComponent>
       </LinearGradient>
     </>
@@ -195,6 +275,8 @@ const styles = StyleSheet.create({
     backgroundColor: colors.moviesBg,
     borderColor: colors.moviesBg,
     borderRadius: 24,
+    flex: 1,
+    alignItems: 'center',
   },
   posterStyle: {
     width: screenWidth / 2 - 12,
@@ -204,5 +286,63 @@ const styles = StyleSheet.create({
   laodingStyle: {
     width: 40,
     height: 40,
+  },
+  menuIcon: {
+    width: 24,
+    height: 24,
+    resizeMode: 'contain',
+    tintColor: 'gray',
+  },
+  modalText: {
+    fontSize: 16,
+    color: colors.LightWhite,
+    fontFamily: fontFamily.rubik_semi_bold,
+    marginLeft: 14,
+  },
+  modalCloseIconContainer: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: colors.lineColor,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 8,
+    right: 12,
+  },
+  modalCloseIcon: {
+    width: 12,
+    height: 12,
+    resizeMode: 'contain',
+    tintColor: 'gray',
+  },
+  modalView: {
+    width: '90%',
+    height: screenHeight / 4,
+    // backgroundColor: colors.black,
+    backgroundColor: '#1C2A34',
+    // alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+  },
+  radio: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    borderColor: colors.lineColor,
+    borderWidth: 1,
+  },
+  fillRadio: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: colors.blue,
+  },
+  modalViewContainer: {
+    paddingHorizontal: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 4,
+    marginBottom: 14,
   },
 });
