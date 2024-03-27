fc61808b6e88f8628a377a5ab862ecfdf0941f5c
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-03-27 12:14:24 +0500

    remove image picker warning

--- app/helper/mediaPicker.js
+++ app/helper/mediaPicker.js
@@ -13,16 +13,13 @@ export const pickImage = () => {
     launchImageLibrary(options, response => {
       if (response.didCancel) {
         console.log('User cancelled image picker');
-        reject(new Error('User cancelled image picker'));
       } else if (response.error) {
         console.log('ImagePicker Error: ', response.error);
-        reject(new Error(response.error));
       } else if (response?.assets[0]?.uri) {
         let uri = response?.assets[0]?.uri;
         resolve(uri);
       } else {
         console.log('Image is not selected!');
-        reject(new Error('Image is not selected'));
       }
     });
   });
