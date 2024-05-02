febb8467bd3576edbe6a976f265d62c6de894ce4
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-05-02 12:38:05 +0500

    complete workout app

--- app/screens/Fitness/components/workOutData.js
+++ app/screens/Fitness/components/workOutData.js
@@ -79,20 +79,16 @@ export const bodyParts = [
 
 export const targetList = [
   {
-    name: 'abductors',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbfnOGS6tG1t1bELbf-uFc9nRlqXJ1-2SDkg&s',
-  },
-  {
-    name: 'abs',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGqHikfhMbGfBp1N2FwDXhSNpn3CfDOemWKQ&s',
-  },
-  {
     name: 'adductors',
     image:
       'https://i0.wp.com/www.muscleandfitness.com/wp-content/uploads/2019/08/Showing-Off-Quads-Leg-Lunge-Bench.jpg?quality=86&strip=all',
   },
+  {
+    name: 'abs',
+    image:
+      'https://i0.wp.com/healthsciencebulletin.com/wp-content/uploads/2023/07/Lower-Ab-Workout.jpg?fit=1640%2C924&ssl=1',
+  },
+
   {
     name: 'biceps',
     image:
@@ -108,19 +104,71 @@ export const targetList = [
     image:
       'https://www.shutterstock.com/image-photo/young-man-sportswear-running-on-600nw-435565456.jpg',
   },
-  {name: 'delts', image: ''},
-  {name: 'forearms', image: ''},
-  {name: 'glutes', image: ''},
-  {name: 'hamstrings', image: ''},
-  {name: 'lats', image: ''},
-  {name: 'levator scapulae', image: ''},
-  {name: 'pectorals'},
-  {name: 'quads', image: ''},
-  {name: 'serratus anterior', image: ''},
-  {name: 'spine', image: ''},
-  {name: 'traps', image: ''},
-  {name: 'triceps', image: ''},
-  {name: 'upper back', image: ''},
+  {
+    name: 'delts',
+    image:
+      'https://img.freepik.com/free-photo/back-view-image-young-sports-man-showing-biceps_171337-8157.jpg?size=626&ext=jpg&uid=R112455233&ga=GA1.1.389427713.1710411350&semt=ais',
+  },
+  {
+    name: 'forearms',
+    image:
+      'https://hips.hearstapps.com/hmg-prod/images/muscle-and-fitness-royalty-free-image-1622119180.jpg?crop=1xw:0.84296xh;center,top',
+  },
+  {
+    name: 'glutes',
+    image:
+      'https://images.unsplash.com/photo-1554284126-aa88f22d8b74?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGdsdXRlcyUyMHdvcmtvdXR8ZW58MHx8MHx8fDA%3D',
+  },
+  {
+    name: 'hamstrings',
+    image:
+      'https://www.trainheroic.com/wp-content/uploads/2021/12/Hamstrings-1.jpg',
+  },
+  {
+    name: 'lats',
+    image:
+      'https://i.pinimg.com/736x/41/38/0a/41380add551a015160ac3240cfbf278a.jpg',
+  },
+  {
+    name: 'levator scapulae',
+    image:
+      'https://st2.depositphotos.com/2631505/10685/i/450/depositphotos_106854654-stock-photo-young-muscular-sport-man-holding.jpg',
+  },
+  {
+    name: 'pectorals',
+    image:
+      'https://t4.ftcdn.net/jpg/01/10/60/93/360_F_110609357_6HZoN2iNERZ2m4bAy77PRNZXNepkG8BC.jpg',
+  },
+  {
+    name: 'quads',
+    image:
+      'https://www.mensjournal.com/.image/t_share/MTk2MTM2OTM1NTEwNTE2ODgx/lunge.jpg',
+  },
+  {
+    name: 'serratus anterior',
+    image:
+      'https://www.dmoose.com/cdn/shop/articles/Main_Image_8fe78d1b-661e-41d7-8b7e-5b141fc50aab.jpg?v=1673703393',
+  },
+  {
+    name: 'spine',
+    image:
+      'https://www.mensjournal.com/.image/t_share/MTk2MTM2MTExNjc2MTM5MDEz/bentoveronearmrow.jpg',
+  },
+  {
+    name: 'traps',
+    image:
+      'https://images.ctfassets.net/8urtyqugdt2l/7lnGQD2yzXPQWlvSuhNt3G/504741a582d5e441775773aa671b374e/traps-exercises-tile-image.jpg',
+  },
+  {
+    name: 'triceps',
+    image:
+      'https://images.unsplash.com/photo-1530822847156-5df684ec5ee1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dHJpY2VwcyUyMHdvcmtvdXR8ZW58MHx8MHx8fDA%3D',
+  },
+  {
+    name: 'upper back',
+    image:
+      'https://cdn.muscleandstrength.com/sites/default/files/field/feature-image/workout/john-meadows-feature.jpg',
+  },
 ];
 
 export const equipmentList = [
@@ -141,102 +189,125 @@ export const equipmentList = [
   },
   {
     name: 'body weight',
-    image: '',
+    image:
+      'https://solostrength.com/cdn/shop/products/FreestandingBodyweightTrainingStationAdjustablePullUpBar-standardheight_1024x1024px_1024x1024.jpg?v=1656380832',
   },
   {
     name: 'bosu ball',
-    image: '',
+    image:
+      'https://artzt.eu/cdn/shop/collections/BOSU-Image-03_ARTZT.webp?v=1692167018&width=2100',
   },
   {
     name: 'cable',
-    image: '',
+    image:
+      'https://watsongym.co.uk/wp-content/uploads/2023/03/Dual-Cable-Adjustable-Pulley-2.jpg',
   },
   {
     name: 'dumbbell',
-    image: '',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/3/3a/Gym_Dumbbells_For_Working_Out_%28193383405%29.jpeg',
   },
   {
     name: 'elliptical machine',
-    image: '',
+    image:
+      'https://www.dmoose.com/cdn/shop/articles/feature-image_13797c43-6167-47c5-96e7-649c1bebe701.jpg?v=1678380726',
   },
   {
     name: 'ez barbell',
-    image: '',
+    image:
+      'https://samsfitness.com.au/wp-content/uploads/2020/09/ATX-EZ-50-CAMO-needle-bearing-ez-curl.jpg',
   },
   {
     name: 'hammer',
-    image: '',
+    image:
+      'https://mirafit.co.uk/wp/wp-content/uploads/2022/07/Mirafit-Gym-Hammer-Workout-1024x684.jpg',
   },
   {
     name: 'kettlebell',
-    image: '',
+    image: 'https://shoppeaks.com/wp-content/uploads/2019/03/KB-Article.jpg',
   },
   {
     name: 'leverage machine',
-    image: '',
+    image:
+      'https://powertec.com/cdn/shop/products/levergym_model4_yellow_wb-177574.jpg?v=1701782014&width=1080',
   },
   {
     name: 'medicine ball',
-    image: '',
+    image:
+      'https://media.post.rvohealth.io/wp-content/uploads/sites/2/2016/02/male-medicine-ball-exercise-thumb.jpg',
   },
   {
     name: 'olympic barbell',
-    image: '',
+    image:
+      'https://steelsupplements.com/cdn/shop/articles/shutterstock_1102455656_1000x.jpg?v=1639309036',
   },
   {
     name: 'resistance band',
-    image: '',
+    image:
+      'https://cdn.mos.cms.futurecdn.net/mxAdUWpt3yPVkr6VGBYDHi-1200-80.jpg',
   },
   {
     name: 'roller',
-    image: '',
+    image:
+      'https://media.istockphoto.com/id/1288561181/photo/man-exercising-in-living-room.jpg?s=612x612&w=0&k=20&c=WA5ZZ_R0njMY7JPpGEf-pLlFa2lPChILYSVmyRmk5Us=',
   },
   {
     name: 'rope',
-    image: '',
+    image:
+      'https://steelsupplements.com/cdn/shop/articles/motherfucker_1731x.jpg?v=1612722912',
   },
   {
     name: 'skierg machine',
-    image: '',
+    image:
+      'https://mensfitnesstoday.com/wp-content/uploads/sites/2/2023/05/skierg2-single-new.jpg?w=900',
   },
   {
     name: 'sled machine',
-    image: '',
+    image:
+      'https://eosfitness.com/wp-content/uploads/2021/09/bigstock-Curly-headed-Sportswoman-Doing-398691131-2.jpg',
   },
   {
     name: 'mith machine',
-    image: '',
+    image:
+      'https://www.eatthis.com/wp-content/uploads/sites/4/2023/04/man-smith-machine-squats.jpg?quality=82&strip=1',
   },
   {
     name: 'stability ball',
-    image: '',
+    image:
+      'https://img.freepik.com/free-photo/female-balance-exercise-bouncing-ball_23-2148353055.jpg?size=626&ext=jpg&ga=GA1.1.389427713.1710411350&semt=ais',
   },
   {
     name: 'stationary bike',
-    image: '',
+    image:
+      'https://movewithascend.com/cdn/shop/articles/dsc09613-1-1693507592406_4d40488e-ba8a-455d-9f02-47696fd0d6e5.jpg?v=1700075987',
   },
   {
     name: 'stepmill machine',
-    image: '',
+    image:
+      'https://i.ytimg.com/vi/K8S_sVxJKLM/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgWihaMA8=&rs=AOn4CLBHt9toGGG1bECyueULFjsuFwly8Q',
   },
   {
     name: 'tire',
-    image: '',
+    image:
+      'https://generationiron.com/wp-content/uploads/2020/09/header-20.jpg',
   },
   {
     name: 'trap bar',
-    image: '',
+    image:
+      'https://i0.wp.com/www.muscleandfitness.com/wp-content/uploads/2017/05/1109-trap-bar-deadlift-lean-muscular.jpg?quality=86&strip=all',
   },
   {
     name: 'upper body ergometer',
-    image: '',
+    image: 'https://ironlifeusa.com/cdn/shop/products/8.jpg?v=1665000686',
   },
   {
     name: 'weighted',
-    image: '',
+    image:
+      'https://cdn.muscleandstrength.com/sites/default/files/field/image/article/gym_rules.jpg',
   },
   {
     name: 'wheel roller',
-    image: '',
+    image:
+      'https://images.pexels.com/photos/8032898/pexels-photo-8032898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
   },
 ];
